b38e90f7b252853bfa093a6671058cff1f270cf9
Summer <sko11003@byui.edu>
2016-06-27 22:24:31 -0500

    Added view pages, mvc folders

--- medreview/index.php
+++ medreview/index.php
@@ -1,32 +1,28 @@
 <!DOCTYPE html>
 <!--My Site Home Page-->
-<html lang="en-us" class="homepage">
+<html lang="en-us" class="calendars">
     <head>
         <meta charset="UTF-8">
         <!-- This Labels the Window/Tab -->
-        <title>HOME | medreview</title>
+        <title>CALENDARS | thewebbspot.com</title>
         <meta name="author" content="Brandon Webb">
-        <meta name="author" content="Summer Smith">        
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="CSS/cssstyles.css" type="text/css" rel="stylesheet" media="screen" />
+        <link href="../CSS/cssstyles.css" type="text/css" rel="stylesheet" media="screen" />
         <meta name="viewport" content="width=device-width, iniHal-scale=1.0, maximum-scale=1.0">
     </head>
-
-
-    <!-- *********************************BODY***************************** -->
-    <body id="home">
-        <nav>
+    <body>
     <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/header.php'; ?>
-        </nav>
-        <div>
-            <main>
-                <div class="info-container">
-                    <h1 class="info-item">This is your spot, so do what you want</h1>
-                </div>
-            </main>
+        
+        <div class="headers">
+            <h1 class="mainheaders">CALENDARS</h1>
         </div>
-         
-<!-- *********************************FOOTER***************************** -->
+        <div class="pagenav">
+        <ul class="pagenavlist">
+            <li><a href="/sitepages/calendarsdad.php">Dad's Calendar</a></li>
+            <li><a href="/sitepages/calendarsmom.php">Mom's Calendar</a></li>
+            <li><a href="/sitepages/calendarskids.php">Kid's Calendar</a></li>
+        </ul>
+        </div>
 
     <footer>
         <?php include $_SERVER['DOCUMENT_ROOT'].'/modules/footer.php'; ?>     
---
+++ medreview/modules/footer.php
@@ -0,0 +1,10 @@
+<div class="fl">
+    <ul class="gful">
+        <li class="footer1"><a href="/sitepages/siteplan.php">Site Plan</a></li>
+        <li class="footer2"><a href="/sitepages/teachingpresentation.php">Teaching Presentation</a></li>
+        <li class="footer3"><a href="/contact/contact.php">Contact Page</a></li>
+        <li><a href="/excercises/exercises.php">Exercises</a></li>
+        <li><a href="/video_tutorials/videos.php">Video Tutorials</a></li>
+    </ul>
+    <script src="/javascript//analytics.js"></script>
+</div>
