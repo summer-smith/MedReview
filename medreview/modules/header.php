9cd68cff7b5acae1b3e85749a8157d7165561df2
Summer <sko11003@byui.edu>
2016-06-30 20:53:00 -0500

    Working CSS from scratch

--- medreview/modules/header.php
+++ medreview/modules/header.php
@@ -2,16 +2,13 @@
     <nav> 
         <div id="menu" class="gh">
             <ul class ="gul">
-                <li class="item1"><a href="/sitepages/music.php">MUSIC</a></li>
-                <li class="item2"><a href="/sitepages/videos.php">VIDEOS</a></li>
-                <li class="item3"><a href="/sitepages/files.php">FILES</a></li>
-                <li class="item4"><a href="/sitepages/photos.php">PHOTOS</a></li>
-                <li class="item5"><a href="/sitepages/recipes.php">RECIPES</a></li>
-                <li class="item6"><a href="/sitepages/calendars.php">CALENDARS</a></li>
+                <li class="head"><a href="/">Review</a></li>
+                <li class="head"><a href="/">About</a></li>
+                <li class="head"><a href="/">Contact</a></li>
             </ul>
         </div>
     </nav>
     <div class="flex-container">
-        <a href="http://thewebbspot.com" class="flex-item">theWebbSpot</a>
+        <a href="/index.php"><img src="../images/logobasic.png" alt="Home"/></a>
     </div>
 </header>                                                                                   
