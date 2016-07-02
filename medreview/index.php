268be4733fbf3a6378b31d5f724f0f517bbcbe2b
Summer <sko11003@byui.edu>
2016-07-01 23:10:30 -0500

    Polished CSS, starting MVC setup

--- medreview/index.php
+++ medreview/index.php
@@ -1,28 +1,25 @@
-<!DOCTYPE html>
-<!--My Site Home Page-->
-<html lang="en-us" class="calendars">
-    <head>
-        <meta charset="UTF-8">
-        <!-- This Labels the Window/Tab -->
-        <title>HOME | medreview</title>
-        <meta name="author" content="Brandon Webb">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../medreview/CSS/cssstyles.css" type="text/css" rel="stylesheet" media="screen" />
-        <meta name="viewport" content="width=device-width, iniHal-scale=1.0, maximum-scale=1.0">
-    </head>
-    <body>
-    <?php include // $_SERVER['DOCUMENT_ROOT'].
-    '/modules/header.php'; ?>
-        
-        <div class="headers">
-            <h1 class="mainheaders">Welcome to Medreview!</h1>
-        </div>
-        
+<?php
+//MAIN CONTROLLER
 
-    <footer>
-        <?php include  // $_SERVER['DOCUMENT_ROOT'].
-        '/modules/footer.php'; ?>     
-    </footer>  
-    </body>
+//For debugging - SUMMER TODO comment out before live launch
+ini_set('display_errors', 1);
+
+//Connect database connectsions
+//BRANDON TODO add require statements
+
+
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL){
+    $action = filter_input(INPUT_GET, 'action');
+}
+
+
+switch( $action ) {
+    case 'Login Page':
+        include('login.php');
+        break;
+     
     
-</html>
+    default:
+        include('/view/home.php');
+}
