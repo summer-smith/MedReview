6b99fdf9c50952e464e3dadead4bc1d5a156e6aa
Summer <sko11003@byui.edu>
2016-07-01 20:52:54 -0500

    --

--- medreview/modules/header.php
+++ medreview/modules/header.php
@@ -1,14 +1,14 @@
 <header>
     <nav> 
-        <div id="menu" class="">
+        <div id="menu" class="flex-container">
             <ul class ="head">
-                <li class="head"><a href="/">Review</a></li>
-                <li class="head"><a href="/">About</a></li>
-                <li class="head"><a href="/">Contact</a></li>
+                <li class="head"><a class="flex-item" href="/">Review</a></li>
+                <li class="head"><a class="flex-item" href="/">About</a></li>
+                <li class="head"><a class="flex-item" href="/">Contact</a></li>
             </ul>
         </div>
     </nav>
     <div class="flex-container">
-        <a href="/index.php"><img src="../images/logobasic.png" alt="Home"/></a>
+        <a href="/index.php"><img src="/images/logabasic.jpg" alt="Home"/></a>
     </div>
 </header>                                                                                   
