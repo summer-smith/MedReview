f466de29e2e5b1fadadbe41a2ed0678e73581fd7
Summer <sko11003@byui.edu>
2016-07-09 19:19:29 -0500

    Improvements to review page

--- medreview/view/doctorreview.php
+++ medreview/view/doctorreview.php
@@ -11,7 +11,7 @@
     <head>
         <meta charset="UTF-8">
         <!-- This Labels the Window/Tab -->
-        <title>HOME | medreview</title>
+        <title>REVIEW | medreview</title>
         <meta name="author" content="Brandon Webb">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
@@ -27,32 +27,37 @@
             <?php include '/modules/sidebar.php'; ?>
            
             <div class="maincontent">
-                <h4>Doctor Review</h4>
-     
-                <h3>Rate your doctor:</h3>
-                <h4>How would you rate your overall experience?</h4>
-                <div class="starcontainer">
-                 <div class="stars">
-                    <form action="">
-                      <input class="star star-5" id="star-5" type="radio" name="star"/>
-                      <label class="star star-5" for="star-5"></label>
-                      <input class="star star-4" id="star-4" type="radio" name="star"/>
-                      <label class="star star-4" for="star-4"></label>
-                      <input class="star star-3" id="star-3" type="radio" name="star"/>
-                      <label class="star star-3" for="star-3"></label>
-                      <input class="star star-2" id="star-2" type="radio" name="star"/>
-                      <label class="star star-2" for="star-2"></label>
-                      <input class="star star-1" id="star-1" type="radio" name="star"/>
-                      <label class="star star-1" for="star-1"></label>
-                    </form>
-                  </div>
-                </div>
-                  
-                <h3>Comments:</h3>
-            <textarea name="comments" id="comments" rows="10" cols="30" 
-                      onblur="" placeholder="">
-            </textarea>
-     
+                <form>
+                    <h4>Doctor Review</h4>
+
+                    <h3>Doctors name:</h3><input name="doctorName" id="doctorName"></input>
+
+                    <h3>Rate your doctor:</h3>
+                    <div class="starcontainer">
+                     <div class="stars">
+                        <form action="">
+                          <input class="star star-5" id="star-5" type="radio" name="star"/>
+                          <label class="star star-5" for="star-5"></label>
+                          <input class="star star-4" id="star-4" type="radio" name="star"/>
+                          <label class="star star-4" for="star-4"></label>
+                          <input class="star star-3" id="star-3" type="radio" name="star"/>
+                          <label class="star star-3" for="star-3"></label>
+                          <input class="star star-2" id="star-2" type="radio" name="star"/>
+                          <label class="star star-2" for="star-2"></label>
+                          <input class="star star-1" id="star-1" type="radio" name="star"/>
+                          <label class="star star-1" for="star-1"></label>
+                        </form>
+                      </div>
+                    </div>
+
+                    <h3>Comments:</h3>
+                    <textarea name="comments" id="comments" rows="10" cols="20" 
+                          onblur="" placeholder="">
+                    </textarea>
+                    
+                    <br><br>
+                    <button type="submit" name="action" class="bttn2" value="TODO">Submit</button>
+                </form>
             </div>
         </div>
         
